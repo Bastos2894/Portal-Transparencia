101289e039786e2d89ce0b039b829ac8eab3ad4e
bastos28 <bastos.pinheiro94@gmail.com>
2026-04-08 22:56:21 -0400

    .env, docker e yaml

--- projeto_bi/Scripts/01_read.py
+++ projeto_bi/Scripts/01_read.py
@@ -5,40 +5,46 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 
 import polars as pl
 import pandas as pd
-from utils.style_planilhas  import style_planilhas
+
+from utils.style_planilhas import style_planilhas
 from utils.data_padrao import clean_dataframe
+from utils.config_loader import load_config
 
+# base
+BASE_DIR = Path(__file__).resolve().parents[1]
 
-# pega raiz do projeto automaticamente
+config = load_config()
 
-BASE_DIR = Path(__file__).resolve().parents[2]
+arquivo = BASE_DIR / config["paths"]["input"]
+output_dir = BASE_DIR / config["paths"]["output"]
+entidade = config["filtro"]["entidade"]
+nome_saida = config["excel"]["nome_saida"]
 
-arquivo = BASE_DIR / "data/planilha_original/transparencia_excel.xls"
+output_dir.mkdir(parents=True, exist_ok=True)
+arquivo_saida = output_dir / nome_saida
 
-#  ler(arquivo .xls do portal vem em html)
+# valida arquivo
+if not arquivo.exists():
+    raise FileNotFoundError(f"Arquivo não encontrado: {arquivo}")
+
+# ler
 df_pd = pd.read_html(arquivo)[0]
-
-# converter para polars
 df = pl.from_pandas(df_pd)
 
+# valida coluna
+if "Entidade" not in df.columns:
+    raise ValueError("Coluna 'Entidade' não encontrada no arquivo.")
+
 # filtro
-df_filtrado = df.filter(
-    pl.col("Entidade") == "SECRETARIA DE ESTADO DE DESENVOLVIMENTO ECONOMICO"
-)
+df_filtrado = df.filter(pl.col("Entidade") == entidade)
 
-#  criar pasta processed autamaticamente
-output_dir = BASE_DIR/ "data"/ "planilha_tratada"
-output_dir.mkdir(parents=True, exist_ok=True)
-
-arquivo_saida = output_dir / "secretaria_desenvolvimento_economico.xlsx"
-
-df_filtrado.write_excel(arquivo_saida)
-
-# limpar dados
+# limpeza
 df_filtrado = clean_dataframe(df_filtrado)
 
-# formatar planilha
+# exportar
+df_filtrado.write_excel(arquivo_saida)
+
+# estilo
 style_planilhas(arquivo_saida)
 
 print("Arquivo salvo em:", arquivo_saida)
-
